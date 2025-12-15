2bf776a59976f5a9182ef837b726ad4c7e604db2
Matheus Nikitiniene Coelho <matheus.nikitiniene2018@gmail.com>
2025-12-15 13:03:46 -0300

    Add files via upload

---
+++ PROJETO_PRODUTIVIDADE/script.py
@@ -0,0 +1,7 @@
+tempo_redes_sociais = float(input("Quantas horas/dia você passa nas redes sociais? "))
+tempo_estudo = float(input("Quantas horas/dia você passa estudando? "))
+
+if tempo_estudo > tempo_redes_sociais:
+    print("Parabéns! Você está investindo no seu futuro.")
+else:
+    print("Você está gastando mais tempo nas redes sociais do que no seu futuro.")
